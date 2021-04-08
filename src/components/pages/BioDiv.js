3b767b867b9b661378b0d30fe3dd06864cd68cfd
kurtp23 <kurt_phillips@live.com>
2021-04-08 13:39:47 -0700

    added footer

--- src/components/pages/BioDiv.js
+++ src/components/pages/BioDiv.js
@@ -7,38 +7,43 @@ class BioDiv extends React.Component {
   //
   render() {
     return (
-      <div className=" container ">
-        <div className="card ">
-          <div className="card-content center-align">
-            <h1>
-              <i className="medium material-icons">BIO</i>
-            </h1>
+      <>
+        <div className=" container ">
+          <div className="card ">
+            <div className="card-content center-align">
+              <h1>
+                <i className="medium material-icons">BIO</i>
+              </h1>
 
-            <img
-              style={{
-                resizeMode: "cover",
-                height: 200,
-                width: 200,
-              }}
-              id="placeholder"
-              alt="Kurt Phillips"
-              src="https://i.postimg.cc/02PT7VbS/kurtpic.jpg"
-            />
+              <img
+                style={{
+                  resizeMode: "cover",
+                  height: 200,
+                  width: 200,
+                }}
+                id="placeholder"
+                alt="Kurt Phillips"
+                src="https://i.postimg.cc/02PT7VbS/kurtpic.jpg"
+              />
 
-            <div className=" col left-align ">
-              Kurt Phillips is a student at the University of Oregon, studying fullstack web
-              development. With a history in sales, Kurt brings a unique insight on how to
-              understand users and how to reach them through UI design and user interactions. With
-              each project Kurt works on, he aims to create a simple and convenient design that
-              engage the user and creates a unique space on the web.
+              <div className=" col left-align ">
+                Kurt Phillips is a student at the University of Oregon, studying fullstack web
+                development. With a history in sales, Kurt brings a unique insight on how to
+                understand users and how to reach them through UI design and user interactions. With
+                each project Kurt works on, he aims to create a simple and convenient design that
+                engage the user and creates a unique space on the web.
+              </div>
+            </div>
+
+            <div className="card-content center-align">
+              <span className="card-title activator grey-text text-darken-4"> </span>
             </div>
           </div>
-
-          <div className="card-content center-align">
-            <span className="card-title activator grey-text text-darken-4"> </span>
-          </div>
         </div>
-      </div>
+        <footer class="page-footer center-align white lighten-2 black-text">
+          © 2021 Copyright Kurt Phillips
+        </footer>
+      </>
     );
   }
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,53 +2,58 @@ import React from "react";
 class Contact extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="card">
-          <div className="center-align">
-            <h1>Contact Form</h1>
-          </div>
-          <div class="row">
-            <form class="col s12">
-              <div class="row">
-                <div class="input-field col s6">
-                  <input id="first_name" type="text" class="validate" />
-                  <label for="first_name">First Name</label>
+      <>
+        <div className="container">
+          <div className="card">
+            <div className="center-align">
+              <h1>Contact Form</h1>
+            </div>
+            <div class="row">
+              <form class="col s12">
+                <div class="row">
+                  <div class="input-field col s6">
+                    <input id="first_name" type="text" class="validate" />
+                    <label for="first_name">First Name</label>
+                  </div>
+                  <div class="input-field col s6">
+                    <input id="last_name" type="text" class="validate" />
+                    <label for="last_name">Last Name</label>
+                  </div>
                 </div>
-                <div class="input-field col s6">
-                  <input id="last_name" type="text" class="validate" />
-                  <label for="last_name">Last Name</label>
-                </div>
-              </div>
 
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="email" type="email" class="validate" />
-                  <label for="email">Email</label>
-                </div>
-                <div class="center-align">
-                  <form class="col s12">
-                    <div class="row">
-                      <div class="input-field col s12">
-                        <i class="material-icons prefix">mode_edit</i>
-                        <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-                        <label for="icon_prefix2">Message</label>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input id="email" type="email" class="validate" />
+                    <label for="email">Email</label>
+                  </div>
+                  <div class="center-align">
+                    <form class="col s12">
+                      <div class="row">
+                        <div class="input-field col s12">
+                          <i class="material-icons prefix">mode_edit</i>
+                          <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                          <label for="icon_prefix2">Message</label>
+                        </div>
                       </div>
-                    </div>
-                  </form>
-                  <button
-                    class="btn waves-effect waves-light center-align"
-                    type="submit"
-                    name="action"
-                  >
-                    Submit
-                    <i class="material-icons right">send</i>
-                  </button>
+                    </form>
+                    <button
+                      class="btn waves-effect waves-light center-align"
+                      type="submit"
+                      name="action"
+                    >
+                      Submit
+                      <i class="material-icons right">send</i>
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+        <footer class="page-footer center-align white lighten-2 black-text">
+          © 2021 Copyright Kurt Phillips
+        </footer>
+      </>
     );
   }
 }
